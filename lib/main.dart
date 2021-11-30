4e5475b410510f3fa9fb3f511da2156c8df6d269
Lorenzo Fiorini <lorenfiorini@gmail.com>
2021-11-30 01:39:30 -0800

    Configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
+import 'src/authentication.dart';
 import 'src/widgets.dart';
 
 void main() {
